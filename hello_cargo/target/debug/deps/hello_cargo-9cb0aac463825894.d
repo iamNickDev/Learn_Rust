97ab5bc22a62a68bc7c7223492efc56574472614
iamNickDev <nandkishoregeete@gmail.com>
2024-02-15 10:06:48 +0530

    Target deleted

--- hello_cargo/target/debug/deps/hello_cargo-9cb0aac463825894.d
+++
@@ -1,5 +0,0 @@
-/Users/nk/Desktop/rust_projects/hello_cargo/target/debug/deps/libhello_cargo-9cb0aac463825894.rmeta: src/main.rs
-
-/Users/nk/Desktop/rust_projects/hello_cargo/target/debug/deps/hello_cargo-9cb0aac463825894.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/nk/Desktop/rust_projects/hello_cargo/target/debug/hello_cargo: /Users/nk/Desktop/rust_projects/hello_cargo/src/main.rs
